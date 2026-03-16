191d640ac28e0ee0876c587f5bcbb1d91fd82b0c
Sun-dried oranges <1113812931@qq.com>
2026-03-16 20:09:42 +0800

    Add files via upload

--- source/psychlua/LuaUtils.hx
+++ source/psychlua/LuaUtils.hx
@@ -525,6 +525,7 @@ class LuaUtils
 	public static function cameraFromString(cam:String):FlxCamera {
 		switch(cam.toLowerCase()) {
 			case 'camgame' | 'game': return PlayState.instance.camGame;
+			case 'cambar' | 'bar': return PlayState.instance.camBar;
 			case 'camhud' | 'hud': return PlayState.instance.camHUD;
 			case 'camother' | 'other': return PlayState.instance.camOther;
 		}
